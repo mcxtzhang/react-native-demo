b20ce5fb402fba0926e649ed5c8f6277fc6fb7ad
mcxtzhang <mcxtzhang@163.com>
2017-05-06 11:23:54 +0800

    [7 Flexbox] flexDirection(方向)  justifyContent（主轴分布方式）  alignItems（次轴分布方式)

--- index7_2_justifyContent.js
+++ index7_2_justifyContent.js
@@ -7,23 +7,17 @@
 
 import React, {Component} from 'react'
 import {AppRegistry, Text, View, StyleSheet} from 'react-native'
-class AlignItems extends Component {
+class JustifyContentTest extends Component {
     render() {
         return (
             <View style={{
                 flex: 1,
                 //flexDirection:'row',
                 flexDirection: 'column',
-                justifyContent: 'space-between',
+                justifyContent: 'space-between'
                 //justifyContent:'center'
                 //justifyContent:'flex-end'
                 //justifyContent:'space-around'
-                //alignItems:'center',
-                //alignItems:'flex-start',
-                alignItems: 'stretch',
-                //注意：要使stretch选项生效的话，子元素在次轴方向上不能有固定的尺寸。
-                // 以下面的代码为例：只有将子元素样式中的width: 50去掉之后，alignItems: 'stretch'才能生效。
-                //alignItems:'flex-end',
             }}>
                 <View style={styles.view1}/>
                 <View style={styles.view2}/>
@@ -37,16 +31,16 @@ class AlignItems extends Component {
 
 const styles = StyleSheet.create({
     view1: {
-        //width: 50,
+        width: 50,
         height: 100,
         backgroundColor: 'blue'
     },
     view2: {
-        //width: 150,
+        width: 150,
         height: 100,
         backgroundColor: 'yellow'
     },
 });
 
 
-AppRegistry.registerComponent("AwesomeProject", ()=> AlignItems);
+AppRegistry.registerComponent("AwesomeProject", ()=> JustifyContentTest);
